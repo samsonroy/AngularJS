e55f78a12b21d49504883f11f8c643bbe2792b62
samsonroy <samsonmaben@gmail.com>
2017-03-24 18:46:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
- var app=angular.module('myApp',[]);
-    app.controller('personController',function($scope) {
-                   $scope.firstName = "";
-                   $scope.lastName = "";
-                   $scope.getFullName=function() {
-        return $scope.firstName + " " + $scope.lastName;
+(function(){
+  'use strict'
+    angular.module('crudapp',[]).controller('MainCtrl',CoreFunction);
+    function CoreFunction(){
+        var viewmodel=this,
+            addressCollection=[],
+            isEditing=false;
+        var add=function(){
+            var newPerson={};
+            if(!angular.equals({},viewmodel.person)){
+                if(isEditing!==false){
+                    addressCollection[isEditing]=viewmodel.person;
+                }
+                else
+                    {
+                        newPerson=viewmodel.person;
+                        addressCollection.push(newPerson);
+                        
+                    }
+                viewmodel.addresses=addressCollection;
+                viewmodel.person={};
+            }
+        };
+          var  edit=function(editPerson){
+                isEditing=addressCollection.indexOf(editPerson);
+                viewmodel.person=angular.copy(editPerson);
+            };
+           var remove=function(removePerson){
+                var index=addressCollection.indexOf(removePerson);
+                addressCollection.splice(index,1);
+                if(addressCollection.length===0){
+                    viewmodel.person={};
+                    viewmodel.addresses=undefined;
+                }
+            };
+          var  reset=function(){
+                viewmodel.person={};
+                viewmodel.search='';
+                isEditing=false;
+            };
+        viewmodel.addClickHandler=function(){add();}
+        viewmodel.editClickHandler=function(editPerson){
+            edit(editPerson);
+        }
+        viewmodel.removeClickHandler=function(removePerson){
+            remove(removePerson);
+            
+        }
+        viewmodel.resetClickHandler=function(){
+            reset();
+        }
     }
-                   });
+})();
